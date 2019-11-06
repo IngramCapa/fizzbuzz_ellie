adacb8319e4c50333a0539c0e79227efac43505e
raluca <raluca.ciucu@gmail.com>
2019-11-06 17:52:37 +0000

    Second commit - multiple of 3s return fizz, else it returns number

--- spec/fizzbuzz_ellie_spec.rb
+++ spec/fizzbuzz_ellie_spec.rb
@@ -4,4 +4,8 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when number divisible by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  
+  it 'returns 4 when 4 is passed in' do
+    expect(fizzbuzz(4)).to eq 4
+    end
 end
